40963d8b8b3540c5d3fc6739583c1cc7c9327751
WrathfulSpatula <stranoj@gmail.com>
2025-05-13 18:48:16 -0400

    QrackStabilizer

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 
-VERSION = "1.42.3"
+VERSION = "1.43.0"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
