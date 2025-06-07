1a2ff8d1f3b06d8abbf9f1245ac6c7333e19f701
WrathfulSpatula <stranoj@gmail.com>
2025-06-07 14:10:23 -0400

    Double epsilon

--- pyqrack/qrack_ace_backend.py
+++ pyqrack/qrack_ace_backend.py
@@ -192,11 +192,11 @@ class QrackAceBackend:
         if "QRACK_FPPOW" in os.environ:
             fppow = int(os.environ.get("QRACK_FPPOW"))
         if fppow < 5:
-            self._epsilon = 2**-11
+            self._epsilon = 2**-10
         elif fppow > 5:
-            self._epsilon = 2**-53
+            self._epsilon = 2**-52
         else:
-            self._epsilon = 2**-24
+            self._epsilon = 2**-23
 
         self._coupling_map = None
 
