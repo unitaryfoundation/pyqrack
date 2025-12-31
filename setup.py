fc0c69ecfdf1dfb5b10d86a9f02a71a96624084c
WrathfulSpatula <stranoj@gmail.com>
2025-12-30 22:59:45 -0500

    (It's past time.) :crying_cat_face:

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: C++",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.4",
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
